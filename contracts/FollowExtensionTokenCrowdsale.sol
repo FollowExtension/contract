8fc06b87c2649c3c077c66c7eefc63585ace372c
Steve Chang <iamstevechang@gmail.com>
2017-10-08 18:46:19 +0900

    add initial mint

--- contracts/FollowExtensionTokenCrowdsale.sol
+++ contracts/FollowExtensionTokenCrowdsale.sol
@@ -6,6 +6,7 @@ import './FollowExtensionToken.sol';
 
 contract FollowExtensionTokenCrowdsale is CappedCrowdsale {
   function FollowExtensionTokenCrowdsale(uint256 _cap, uint256 _startTime, uint256 _endTime, uint256 _rate, address _wallet) CappedCrowdsale(_cap) Crowdsale(_startTime, _endTime, _rate, _wallet) {
+    token.mint(wallet, 5000000); // 2,000,000(for team) + 3,000,000(for official sale)
   }
 
   function createTokenContract() internal returns (MintableToken) {
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,9 @@
 const FollowExtensionTokenCrowdsale = artifacts.require("./FollowExtensionTokenCrowdsale.sol")
 
 module.exports = function(deployer, network, accounts) {
-  const startTime = web3.eth.getBlock(web3.eth.blockNumber).timestamp + 100 // one second in the future
-  const endTime = startTime + (86400 * 10) // 10 days
+  const startTime = web3.eth.getBlock(web3.eth.blockNumber).timestamp + 100 // 100 second in the future
+  const endTime = startTime + (86400 * 7) // 7 days
   const rate = new web3.BigNumber(1000)  // 1 ETH = 1000 TOKEN
-  console.log("TEST: " + accounts + " " + network);
   const wallet = accounts[0]
 
   deployer.deploy(FollowExtensionTokenCrowdsale, 2000000000000000000000, startTime, endTime, rate, wallet)
